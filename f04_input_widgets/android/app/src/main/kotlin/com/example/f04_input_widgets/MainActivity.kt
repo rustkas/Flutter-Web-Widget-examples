ccbc18452f594411b60da1b12a58b9239aaf71ad
Anatolii Kosorukov <java1cprog@yandex.ru>
2020-10-07 19:42:26 +0200

    Flutter Widget | 04 | Input Widgets | Form, TextFormField

---
+++ f04_input_widgets/android/app/src/main/kotlin/com/example/f04_input_widgets/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.f04_input_widgets
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ f04_input_widgets/lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: Color(0xFF832685),
+        primaryColorLight: Color(0xFFC81379),
+        accentColor: Colors.black,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+@immutable
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final String title = 'Input Widgets';
+  final formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextFormField(
+                cursorColor: Colors.purple,
+                decoration: InputDecoration(
+                  hintText: 'Enter First Name',
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please, enter first name';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                cursorColor: Colors.purple,
+                decoration: InputDecoration(
+                  hintText: 'Enter Last Name',
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please, enter last name';
+                  }
+                  return null;
+                },
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Center(
+                  child: RaisedButton(
+                      onPressed: () {
+                        if (formKey.currentState.validate()) {
+                          //is valid
+                        }
+                      },
+                      child: Text('Submit')),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
