e060e7322b09182875e6bd81dc915b38903ba360
Anatolii Kosorukov <java1cprog@yandex.ru>
2020-10-08 22:12:32 +0200

    Add GridView example

---
+++ w11_GridView/lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:w11_GridView/images.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: Color(0xFF832685),
+        primaryColorLight: Color(0xFFC81379),
+        accentColor: Colors.black,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String title = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+        centerTitle: true,
+      ),
+      body: GridView.count(
+        crossAxisCount: 3,
+        children: List.generate(50, (index) {
+          return Container(
+            margin: EdgeInsets.all(32),
+            child: Card(
+              elevation: 10,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5)),
+              child: Center(
+                child:
+                    // Image(
+                    //   image: AssetImage(captainAmerica),
+                    //   height: 100,
+                    //   width: 100,
+                    //   fit: BoxFit.cover,
+                    // )
+                    Text(
+                  "$index",
+                  style: TextStyle(fontSize: 32),
+                ),
+              ),
+            ),
+          );
+        }),
+      ),
+    );
+  }
+}
