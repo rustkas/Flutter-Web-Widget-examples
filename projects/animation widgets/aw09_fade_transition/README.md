a247f7efab9429d67d95b5b56f4e01c95925c0e3
Anatolii Kosorukov <java1cprog@yandex.ru>
2020-11-14 13:58:38 +0200

    Rename projects, add aw09_fade_transition

---
+++ projects/animation widgets/aw09_fade_transition/README.md
@@ -0,0 +1,5 @@
+# Fade Transition
+## Tween, AnimationStatus, Animation
+
+- [Video tutorial](https://youtu.be/zB7QxBsnylo)
+- [Author's repository](https://github.com/TheTechDesigner/FadeTransition)
---
+++ projects/animation widgets/aw09_fade_transition/lib/main.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'images.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: Color(0xFF832685),
+        primaryColorLight: Color(0xFFC81379),
+        accentColor: Colors.black,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  String title = 'Fade Transition';
+
+  AnimationController _controller;
+  Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller =
+        AnimationController(vsync: this, duration: Duration(seconds: 2));
+
+    _animation = Tween(begin: 0.0, end: 1.0).animate(_controller);
+
+    _animation.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _controller.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        _controller.forward();
+      }
+    });
+
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  final list = [venom, spiderMan, drStrange];
+  int imageIndex = 0;
+  String getImageLink() {
+    String imageLink;
+    imageLink = list[imageIndex];
+
+    return imageLink;
+  }
+
+  AssetImage image;
+  @override
+  Widget build(BuildContext context) {
+    image = AssetImage(getImageLink());
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+        centerTitle: true,
+      ),
+      body: GestureDetector(
+        onTap: () {
+          setState(() {
+            imageIndex++;
+            if (imageIndex == list.length) {
+              imageIndex = 0;
+            }
+            image = AssetImage(getImageLink());
+          });
+        },
+        child: FadeTransition(
+          opacity: _animation,
+          child: Center(
+            child: Image(image: image, width: 300.0),
+          ),
+        ),
+      ),
+    );
+  }
+}
